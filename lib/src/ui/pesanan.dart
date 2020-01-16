2034141201bd2cd247a540062930d2c2f513629a
ron2618 <45929774+ron2618@users.noreply.github.com>
2020-01-16 04:30:03 -0800

    mmmm

--- lib/src/ui/pesanan.dart
+++ lib/src/ui/pesanan.dart
@@ -150,7 +150,7 @@ class DetailPesanan extends StatefulWidget {
 class _DetailPesananState extends State<DetailPesanan> {
      //getdetailpem
      
-  
+  final myController = TextEditingController();
   Future<String> getDetailpem(String order) async {
     var response = await http.post(
         //Encode the url
@@ -192,6 +192,79 @@ String merchant_id = map1['merchant_id'];
  
   }
 //getjson
+Future<String> tambahnego() async {
+    var response = await http.post(
+        //Encode the url
+
+        Uri.encodeFull(
+            'https://siplah.mascitra.co.id/api/sekolah/pesanan/nego_tambah'),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "API-App": "siplah_jpmall.id",
+          "Api-Key": "4P1_7Pm411_51p114h",
+          "API-Token": "575696f2ed816e00edbfa90f917c6f757e5ce05a"
+        },
+        body: {
+          "transaksi_produk_id": "9",
+           "harga_nego" : myController.text,
+           "user_id" : "1",
+        });
+    //print(response.body);
+    setState(() {
+      // ignore: deprecated_member_use
+      getNego();
+    });
+  }
+  String idnego;
+Future<String> setujui(String idvoid) async {
+    var response = await http.post(
+        //Encode the url
+
+        Uri.encodeFull(
+            'https://siplah.mascitra.co.id/api/mitra/penjualan/nego_setuju'),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "API-App": "siplah_jpmall.id",
+          "Api-Key": "4P1_7Pm411_51p114h",
+          "API-Token": "575696f2ed816e00edbfa90f917c6f757e5ce05a"
+        },
+        body: {
+         
+           "nego_id" :  idvoid,
+           "user_id" : "4",
+        });
+    //print(response.body);
+    setState(() {
+      // ignore: deprecated_member_use
+     
+    });
+  }
+List negosiasi;
+ Future<String> getNego() async {
+    var response = await http.post(
+        //Encode the url
+
+        Uri.encodeFull(
+            'https://siplah.mascitra.co.id/api/sekolah/pesanan/nego_tampil'),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "API-App": "siplah_jpmall.id",
+          "Api-Key": "4P1_7Pm411_51p114h",
+          "API-Token": "575696f2ed816e00edbfa90f917c6f757e5ce05a"
+        },
+        body: {
+          "transaksi_produk_id": "9",
+    
+        });
+    //print(response.body);
+    setState(() {
+      // ignore: deprecated_member_use
+      var convertDataToJson = json.decode(response.body);
+      negosiasi = convertDataToJson['Data'];
+   
+
+    });
+  }
 int o;
  String nama;
   List data,produk,mitra;
@@ -219,7 +292,7 @@ int o;
       mitra = convertDataToJson['Data'][0]['mitra'];
       produk = convertDataToJson['Data'][0]['mitra'][0]['produk'];
   o=produk.length.toInt();
-
+getNego();
     });
   }
 
@@ -241,12 +314,150 @@ int o;
     super.initState();
     getCredential();
   }
+void shownego() {
+    // flutter defined function
+    
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
 
+          title: new Text("Negosiasi"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                height: 150,
+                width:  MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                
+                  itemCount: negosiasi.length,
+                  itemBuilder: (context, i) {
+                    int a = i+1;
+                    return Column(
+
+                      children: <Widget>[
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.grey,
+                          height: 2,
+                        ),
+                         SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Text(a.toString()+"."),
+                            SizedBox(width: 10,),
+                            Container(child: Text(negosiasi[i]['nego'] +" ") ),
+                            SizedBox(width: 50,),
+                           Column(
+                              children: <Widget>[
+                              negosiasi[i]['user_setuju'] == "1"  ?   Container(
+                                  height: 30,
+                                  width: 150,
+                                  child: Center(child: Text("sekolah : Setuju",style: TextStyle(color: Colors.white),)),
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                ):Container(
+                                  height: 30,
+                                  width: 150,
+                                  child: Center(child: Text("sekolah : -",style: TextStyle(color: Colors.white),)),
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                ),
+                                SizedBox(height: 2,),
+                                   negosiasi[i]['mitra_setuju'] == "1"  ? Container(
+                                  height: 30,
+                                  width: 150,
+                                  child: Center(child: Text("mitra : setuju",style: TextStyle(color: Colors.white),)),
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                ): GestureDetector( 
+                                  onTap: (){
+                                   
+                                    setujui( negosiasi[i]['id']);
+                                  },
+                                      child:Container(
+                                  height: 30,
+                                  width: 150,
+                                  child: Center(child: Text("mitra : -",style: TextStyle(color: Colors.white),)),
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlue,
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                ),)
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  }),
+              ),
+          
+           Row(
+             children: <Widget>[
+               Container(
+                 width: 150,
+                 child: new TextFormField(
+                   controller: myController,
+                      keyboardType: TextInputType.number, // Use email input type for emails.
+                      decoration: new InputDecoration(
+                        hintText: '100000',
+                        labelText: 'Nego Berapa'
+                      ),),
+               ),
+                 SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: (){
+       tambahnego();
+                  },
+                                  child: Container(
+                                    height: 40,
+                                    width: 100,
+                                    child: Center(child: Text("Simpan",style: TextStyle(color: Colors.white),)),
+                                    decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(50)
+                                    ),
+                                  ),
+                )
+             ],
+           ),
+              
+            ],
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
   //detailpesanan
   void _showAlertupload(BuildContext context,String payment_code,String store,String transaction_time,String gross_amount,String currency,String order_id,String payment_type,String signature_key,String status_code,String transaction_id,String transaction_status,String status_message,String merchant_id
 ) {
   
-  
+   
     showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -508,6 +719,7 @@ int o;
           ),
           Container(
             height: 80,
+            width: MediaQuery.of(context).size.width,
             child: ListView.builder(
                 physics: NeverScrollableScrollPhysics(),
                 itemCount: data.length,
@@ -549,7 +761,9 @@ int o;
           ),
           Container(
             height: 50,
-            child: Row(
+            width: MediaQuery.of(context).size.width,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
               children: <Widget>[
                 SizedBox(width: 5,),
                 Padding(
@@ -604,32 +818,37 @@ int o;
                   int harga=int.parse(produk[i]['harga']);
                 
                   int sub = jumlah*harga;
-                   return Card(
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(child: Text(a.toString())),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: 40,
-                              height: 40,
-                              child: Image.network(produk[i]['foto']!=null?produk[i]['foto']:
-                                  'https://siplah.mascitra.co.id/assets/images/user.ico')),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: MediaQuery.of(context).size.width / 13,
-                              child: Text(produk[i]['nama'])),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: MediaQuery.of(context).size.width / 7,
-                              child: Html(
+                   return Container(
+                     height: 100,
+                     width: MediaQuery.of(context).size.width,
+                     child: Card(
+                       
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(child: Text(a.toString())),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                                width: 40,
+                                height: 40,
+                                child: Image.network(produk[i]['foto']!=null?produk[i]['foto']:
+                                    'https://siplah.mascitra.co.id/assets/images/user.ico')),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                                width: MediaQuery.of(context).size.width / 13,
+                                child: Text(produk[i]['nama'])),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                                width: MediaQuery.of(context).size.width / 7,
+                                child: Html(
             data: produk[i]['deskripsi'],
             //Optional parameters:
             onLinkTap: (url) {
@@ -639,47 +858,48 @@ int o;
               if (node is dom.Element) {
                 switch (node.localName) {
                   case "custom_tag":
-                    return Column(children: children);
+                      return Column(children: children);
                 }
               }
             },
           ),//Text(produk[i]['deskripsi'])),
-                        )),
+                          )),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                                width: MediaQuery.of(context).size.width / 20,
+                                child: Text(produk[i]['jumlah'])),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                                width: MediaQuery.of(context).size.width / 10,
+                                child: Text(produk[i]['harga'])),
+                          ),
                         Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: MediaQuery.of(context).size.width / 20,
-                              child: Text(produk[i]['jumlah'])),
+                          padding: const EdgeInsets.all(20),
+                          child: GestureDetector(
+                            onTap: (){
+shownego();
+                            },
+                                                      child: Container(
+                              width: 100,
+                            
+                              color: Colors.blueAccent,
+                              child: Center(child: Text("NEGO",style: TextStyle(color: Colors.white),)),
+                            ),
+                          ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: MediaQuery.of(context).size.width / 10,
-                              child: Text(produk[i]['harga'])),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: MediaQuery.of(context).size.width / 7,
-                              child: produk[i]['nego']!='1'? RaisedButton(
-                                  color: Colors.blueGrey,
-                                onPressed: () {},
-                                child: Text("NEGO",style: TextStyle(color: Colors.white),),):RaisedButton(
-                                  color: Colors.blue,
-                                onPressed: () {},
-                                child: Text("NEGO",style: TextStyle(color: Colors.white)),
-                              )),
-                        ),
-                        
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: MediaQuery.of(context).size.width / 7,
-                              child: Text(sub.toString())),
-                        )
-                      ],
-                    ),
-                  );
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                                width: MediaQuery.of(context).size.width / 7,
+                                child: Text(sub.toString())),
+                          )
+                        ],
+                      ),
+                  ),
+                   );
                 }),
           ),
           Container(
